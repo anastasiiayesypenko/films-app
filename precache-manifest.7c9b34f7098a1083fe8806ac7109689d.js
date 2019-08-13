e107b8b6088bc3667a441c48d8e0e89c56b32d02
Anastasia <nastiyayesypenko@ukr.net>
2019-08-13 15:52:00 +0300

    Updates

---
+++ precache-manifest.7c9b34f7098a1083fe8806ac7109689d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2b46a1994471a46f107ca6edf7a145",
+    "url": "/films-app/index.html"
+  },
+  {
+    "revision": "9760aa46d3d797dc994f",
+    "url": "/films-app/static/css/9.39017165.chunk.css"
+  },
+  {
+    "revision": "2fac45586c17467a338c",
+    "url": "/films-app/static/css/favorites-page.fb61b017.chunk.css"
+  },
+  {
+    "revision": "c270809deab05d519707",
+    "url": "/films-app/static/css/home-page.8b03e553.chunk.css"
+  },
+  {
+    "revision": "3188fbe4c7d8816540ee",
+    "url": "/films-app/static/css/main.6f7816a2.chunk.css"
+  },
+  {
+    "revision": "7e9f9dd3277270c5ad7e",
+    "url": "/films-app/static/css/user-list.fb61b017.chunk.css"
+  },
+  {
+    "revision": "5bb2de9a8b4dbf66bc6e",
+    "url": "/films-app/static/css/watched-page.fb61b017.chunk.css"
+  },
+  {
+    "revision": "ab49c28db0a49b6f4e77",
+    "url": "/films-app/static/js/0.cdfb854d.chunk.js"
+  },
+  {
+    "revision": "fce3bc4d9f64114fd59d",
+    "url": "/films-app/static/js/8.37bda633.chunk.js"
+  },
+  {
+    "revision": "9760aa46d3d797dc994f",
+    "url": "/films-app/static/js/9.10970ec4.chunk.js"
+  },
+  {
+    "revision": "2fac45586c17467a338c",
+    "url": "/films-app/static/js/favorites-page.890f6bcb.chunk.js"
+  },
+  {
+    "revision": "ea38d369410a89d36e4e",
+    "url": "/films-app/static/js/film-page.9a4ae852.chunk.js"
+  },
+  {
+    "revision": "c270809deab05d519707",
+    "url": "/films-app/static/js/home-page.2724f233.chunk.js"
+  },
+  {
+    "revision": "3188fbe4c7d8816540ee",
+    "url": "/films-app/static/js/main.c451f41f.chunk.js"
+  },
+  {
+    "revision": "70d258515a948874ad00",
+    "url": "/films-app/static/js/runtime~main.edbe5328.js"
+  },
+  {
+    "revision": "7e9f9dd3277270c5ad7e",
+    "url": "/films-app/static/js/user-list.b48a8642.chunk.js"
+  },
+  {
+    "revision": "5bb2de9a8b4dbf66bc6e",
+    "url": "/films-app/static/js/watched-page.6872f269.chunk.js"
+  },
+  {
+    "revision": "e415b16110adf49f41702865fa0fbb53",
+    "url": "/films-app/static/media/arrow.e415b161.svg"
+  },
+  {
+    "revision": "7e6f179d8db49f0934a7ed5118bce62a",
+    "url": "/films-app/static/media/busket.7e6f179d.svg"
+  },
+  {
+    "revision": "e51505fad5e841abee174226fcc4dc84",
+    "url": "/films-app/static/media/doubleArrow.e51505fa.svg"
+  },
+  {
+    "revision": "2591dafb9fddc901cfcd1a6c9db4c754",
+    "url": "/films-app/static/media/eye.2591dafb.svg"
+  },
+  {
+    "revision": "57f5bbb19a954574555d58a91f9f2af7",
+    "url": "/films-app/static/media/heart.57f5bbb1.svg"
+  },
+  {
+    "revision": "f5906926038ef74c4c0f8d3974cccfb7",
+    "url": "/films-app/static/media/search.f5906926.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/films-app/precache-manifest.ba77adb4ca4d6d7004858f50160e5ee1.js"
+  "/films-app/precache-manifest.7c9b34f7098a1083fe8806ac7109689d.js"
 );
 
 self.addEventListener('message', (event) => {
